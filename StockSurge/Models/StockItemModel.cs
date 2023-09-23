7b4d9f2b42ec9e4523769f1e51dea4d6484787f4
rushp28 <CB011487@students.apiit.lk>
2023-09-23 13:45:43 +0530

    // Initial Commit

---
+++ StockSurge/Models/StockItemModel.cs
@@ -0,0 +1,29 @@
+﻿namespace StockSurge.Models; 
+
+public class StockItemModel {
+    private readonly string _code;
+    private readonly string _name;
+    private int _quantityInStock;
+
+    public StockItemModel(int quantityInStock, string code, string name) {
+        _code = code;
+        _name = name;
+        _quantityInStock = quantityInStock;
+    }
+    
+    public string GetCode() {
+        return _code;
+    }
+
+    public string GetName() {
+        return _name;
+    }
+
+    public int GetQuantityInStock() {
+        return _quantityInStock;
+    }
+
+    public void SetQuantityInStock(int newQuantityInStock) {
+        _quantityInStock = newQuantityInStock;
+    }
+}
